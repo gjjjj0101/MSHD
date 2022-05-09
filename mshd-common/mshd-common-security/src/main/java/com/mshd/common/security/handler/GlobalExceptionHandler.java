8c3e5c73a19cdbe576147f6b88d1055683cd6fea
高健 <1209640759@qq.com>
2022-05-09 22:31:19 +0800

    feat: add Exception Handler

--- mshd-common/mshd-common-security/src/main/java/com/mshd/common/security/handler/GlobalExceptionHandler.java
+++ mshd-common/mshd-common-security/src/main/java/com/mshd/common/security/handler/GlobalExceptionHandler.java
@@ -1,11 +1,12 @@
 package com.mshd.common.security.handler;
 
+import com.mshd.common.core.constant.HttpStatus;
+import com.mshd.common.core.exception.FileException;
 import com.mshd.common.core.web.domain.RequestResponseBody;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 import javax.servlet.http.HttpServletRequest;
-import com.mshd.common.core.exception.FileException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,12 +14,7 @@ public class GlobalExceptionHandler {
      * 文件操作异常
      */
     @ExceptionHandler(FileException.class)
-    public RequestResponseBody handleNotPermissionException(FileException e, HttpServletRequest request)
-    {
-        String requestURI = request.getRequestURI();
-
-        //To-Do
-        return new RequestResponseBody();
-        //return RequestResponseBody.error();
+    public RequestResponseBody handleNotPermissionException(FileException e, HttpServletRequest request) {
+        return RequestResponseBody.error(HttpStatus.FORBIDDEN, "无权限访问");
     }
 }
