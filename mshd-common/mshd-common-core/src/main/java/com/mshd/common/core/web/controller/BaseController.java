67b6a168d788540988c66b1a6710a5128430d377
高健 <1209640759@qq.com>
2022-05-09 22:28:42 +0800

    feat: BaseController and TestController

--- mshd-common/mshd-common-core/src/main/java/com/mshd/common/core/web/controller/BaseController.java
+++ mshd-common/mshd-common-core/src/main/java/com/mshd/common/core/web/controller/BaseController.java
@@ -1,4 +1,38 @@
 package com.mshd.common.core.web.controller;
 
+import com.mshd.common.core.web.domain.RequestResponseBody;
+
 public class BaseController {
+
+    protected RequestResponseBody toRequestResponseBody(boolean result) {
+        return result ? success() : error();
+    }
+
+    /**
+     * 返回成功
+     */
+    public RequestResponseBody success() {
+        return RequestResponseBody.success();
+    }
+
+    /**
+     * 返回失败消息
+     */
+    public RequestResponseBody error() {
+        return RequestResponseBody.error();
+    }
+
+    /**
+     * 返回成功消息
+     */
+    public RequestResponseBody success(String message) {
+        return RequestResponseBody.success(message);
+    }
+
+    /**
+     * 返回失败消息
+     */
+    public RequestResponseBody error(String message) {
+        return RequestResponseBody.error(message);
+    }
 }
--- mshd-visual/src/main/java/com/mshd/visual/controller/TestController.java
+++ mshd-visual/src/main/java/com/mshd/visual/controller/TestController.java
@@ -1,4 +1,17 @@
 package com.mshd.visual.controller;
 
-public class TestController {
+import com.mshd.common.core.web.controller.BaseController;
+import com.mshd.common.core.web.domain.RequestResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController extends BaseController {
+
+    @GetMapping("/hello/{string}")
+    public RequestResponseBody testName(@PathVariable("string") String s){
+        return this.success("My name is" + s);
+    }
+
 }
