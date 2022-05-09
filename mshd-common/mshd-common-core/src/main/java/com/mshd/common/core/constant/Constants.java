993dc767be4efedc40e5d71fe0a36c678a0e5f8f
高健 <1209640759@qq.com>
2022-05-09 22:29:50 +0800

    feat: add constants

--- mshd-common/mshd-common-core/src/main/java/com/mshd/common/core/constant/Constants.java
+++ mshd-common/mshd-common-core/src/main/java/com/mshd/common/core/constant/Constants.java
@@ -1,13 +1,8 @@
 package com.mshd.common.core.constant;
 
 public class Constants {
-    /**
-     * 成功标记
-     */
+
     public static final Integer SUCCESS = 200;
 
-    /**
-     * 失败标记
-     */
     public static final Integer FAIL = 500;
 }
